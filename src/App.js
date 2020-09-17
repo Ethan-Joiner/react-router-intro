13470ea4a2399ef6828f090180b2147a097387f2
Ethan <ethan.joiner@gmail.com>
2020-09-17 14:30:30 -0500

    initial setup of components complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Shop from './components/Shop';
+import Nav from './components/Nav';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+  return (
+    <nav>
+        <h3>Logo</h3>
+        <li>About</li>
+        <li>Shop</li>
+    </nav>
+  );
+}
+
+export default Nav;
